ee587c4b929cea5a3e778542dfda3613661e4f74
NycolasLuiz <nycolasluizsamuel@gmail.com>
2026-07-12 19:55:20 -0300

    git commit -m "fix: corrige nomes de atributos nos prints do main.py"

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from servicos.pedido_service import novo_pedido
-
+import json
 pedido = novo_pedido()
 
 print("\nPedido criado com sucesso!")
@@ -7,6 +7,6 @@ print("\nPedido criado com sucesso!")
 print(f"Cliente: {pedido.cliente.nome}")
 print(f"Telefone: {pedido.cliente.telefone}")
 print(f"Produto: {pedido.produto.nome}")
-print(f"Valor: R$ {pedido.valor_total:.2f}")
+print(f"Valor: R$ {pedido.produto.valor:.2f}")
 print(f"Entrega: {pedido.periodo_entrega}")
 print(f"Pagamento: {pedido.pagamento}")
