4b1274d5cc46cedb4578511b78bab930bd59ae9b
NycolasLuiz <nycolasluizsamuel@gmail.com>
2026-07-24 21:32:58 -0300

    refactor: adiciona validação de entrada em json vazio e ajusta nomes de variáveis

--- repositories/cliente_repository.py
+++ repositories/cliente_repository.py
@@ -2,19 +2,18 @@ import json
 
 def salvar(cliente):
     PASTA = "data/clientes.json"
-    dados_cleintes = cliente.to_dict()
-    with open (PASTA, "r",encoding="utf-8") as arquivos:
-        existe_cliente = json.load(arquivos)
-        existe_cliente.append(dados_cleintes)
-    with open(PASTA, "w", encoding="utf-8")as arquivos:
-        json.dump (existe_cliente, arquivos, indent=4, ensure_ascii=False) 
-        
-def listar_clientes ():
-    PASTA = "data/clientes.json"
-    with open ( PASTA,"r",encoding="utf-8") as arquivos:
-        clientes = json.load(arquivos)
-        return clientes
-    
+    novo_cliente = cliente.to_dict()
+
+    with open(PASTA, "r", encoding="utf-8") as arquivos:
+        try:
+            clientes = json.load(arquivos)
+        except json.JSONDecodeError:
+            clientes = []
+
+    clientes.append(novo_cliente)
+
+    with open(PASTA, "w", encoding="utf-8") as arquivos:
+        json.dump(clientes, arquivos, indent=4, ensure_ascii=False)
 
 def buscar_telefone(telefone):
     PASTA = "data/clientes.json"
