1afbdb9bd4208c46945642c3d1067efd5fff4c14
NycoalsLuiz <nycolasluizsamuel@gmail.com>
2026-07-25 14:06:26 -0300

    feat: adiciona menu de produtos

---
+++ menu/menu_produtos.py
@@ -0,0 +1,27 @@
+
+def menuDeProdutos():
+    while True:
+        print("="*62)
+        print("-----------ESCOLHA A OPÇÃO DESEJADA---------------")
+        print("-----------1 CADASTRAR PRODUTO--------------------")
+        print("-----------2 MOSTRAR PRODUTOS DISPONÍVEIS---------")
+        print("-----------3 ALTERAR PRODUTO----------------------")
+        print("-----------4 EXCLUIR PRODUTO----------------------")
+        print("-----------0 SAIR-------------------------------- ")
+
+        opcao  = input("INFORME A OPÇÃO DESEJADA: ")
+
+        if opcao == "1":
+            print("FUNÇÃO EM DESENVOLVIMENTO")
+        elif opcao == "2":
+            print("FUNÇÃO EM DESENVOLVIMENTO")
+        elif opcao == "3":
+            print("FUNÇÃO EM DESENVOLVIMENTO") 
+        elif opcao == "4": 
+            print("FUNÇÃO EM DESENVOLVIMENTO")
+        elif opcao == "0":
+            print("SAINDO....")
+            break
+        else:
+            print("ERRO!!, ECOLHA UMA OPÇÃO VÁLIDA")
+        
