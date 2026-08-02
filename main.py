55be3a8a025539d5155fa126eb45c03cbd7254c1
NycolasLuiz <nycolasluizsamuel@gmail.com>
2026-08-02 17:04:23 -0300

    feat: adiciona menu de escolha na main

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 from menu.menu_clientes import menu_principal
 from menu.menu_produtos import menuDeProdutos
-menu_principal()
-menuDeProdutos()
+
+while True:
+    print("="*62)
+    print(" 1 - MENU/CLIENTES")
+    print(" 2 - MENU/PRODUTOS")
+    print(" 3 - MENU/PEDIDO")
+    print(" 4 - MENU/COMISSÃO")
+    print(" 5 - VOLTAR")
+    print(" 0 - SAIR")
+    print("="*62)
+    
+    opcao = input("OPÇÃO DESEJADA:")
+
+    if opcao == "1":
+        menu_principal()
+    elif opcao == "2":
+        menuDeProdutos()
+    elif opcao == "3":
+        print("FUNÇÃO EM DESENVOLVIMENTO")    
+    elif opcao == "4":
+        print("FUNÇÃO EM DESENVOLVIMENTO")
+    elif opcao == "5":
+        continue    
+    elif opcao == "0":
+        print("SAINDO....")
+        break
+    else:
+        print("ERRO! ESCOLHA UMA OPÇÃO VÁLIDA")
+        continue
