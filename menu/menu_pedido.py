c169e7772a43bddbff7c9b88b14851ff8c90647e
NycoalsLuiz <nycolasluizsamuel@gmail.com>
2026-08-11 17:13:41 -0300

    feat: adiciona seleção de produto no cadastro de pedido

--- menu/menu_pedido.py
+++ menu/menu_pedido.py
@@ -3,9 +3,24 @@ from services import pedido_service
 
 def cadastro_novoPedido():
     
-    clienteTelefone = (input("INFORME O TELEFONE DO CLIENTE: "))
-    registroPedido = pedido_service.cadastro_novoPedido(clienteTelefone)
+    clienteTelefone = input("INFORME O TELEFONE DO CLIENTE: ")
+    print("="*62)
+    print("1 - INFORMAR MODELO")
+    print("2 - VER OPÇÕES DISPONÍVEIS")
+    print("="*62)
+
+    opcao = input("INFORME A OPÇÃO DESEJADA: ")
+
+    if opcao == "1":
+        produto = input("INFORME O NOME DO PRODUTO DESEJADO: ")
+    elif opcao == "2":
+        print("EM DESENVOLVIMENTO")
+    else: 
+        print("ESCOLHA UMA OPÇÃO VÁLIDA")
+        return opcao
+    
+    registroPedido = pedido_service.cadastro_novoPedido(clienteTelefone,produto)
     if registroPedido:
-        print("LOCALIZADO/SENDO IMPLEMENTADO"   )
+        print(f"LOCALIZADO/SENDO IMPLEMENTADO {produto}"   )
     else:
         print("NÃO LOCALIZADO/SENDO IMPLEMENTADO")
--- services/pedido_service.py
+++ services/pedido_service.py
@@ -1,10 +1,11 @@
 from models.pedido import Pedido
 from repositories import pedido_repository
 from repositories import cliente_repository
+from repositories import produto_repository
 from datetime import date
 
 
-def cadastro_novoPedido(clienteTelefone):
+def cadastro_novoPedido(clienteTelefone,produto):
 
     id_pedido = pedido_repository.geradorDeId()
 
@@ -12,13 +13,15 @@ def cadastro_novoPedido(clienteTelefone):
 
     busca_cliente = cliente_repository.buscar_telefone(clienteTelefone)
 
+    busca_produto = produto_repository.puxando_produto(produto)
+
     if not busca_cliente:
         return None
 
     novoPedido = Pedido(
         id_pedido,
         busca_cliente,
-        [],
+        busca_produto,
         0,
         data_Pedido,
         None
